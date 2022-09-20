577c3160a2db5f6cbd65dbc30ebfbaf930dcb3e2
Caballero018 <alejoxcaballero18@gmail.com>
2022-09-19 22:32:28 -0500

    Complete this source code to print Welcome to Holberton School!

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Welcome to"
-str2 = "Holberton School!"
-str3 = " "
-print(f"{str1 + str3 + str2}")
+str1 = "Holberton"
+str2 = "School"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(f"Welcome to {str1}!")
