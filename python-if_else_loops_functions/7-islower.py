be30b860053838665acb8a9b475b3ce0bc2d8527
Caballero018 <alejoxcaballero18@gmail.com>
2022-09-21 10:07:54 -0500

    Function that checks for lowercase character.

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+from msilib.schema import Error
+
+
 def islower(c):
     s, d = 'a', 'z'
     if not (c <= d and c >= s):
         return False
+    elif c == '':
+        return Error
     else:
         return True
