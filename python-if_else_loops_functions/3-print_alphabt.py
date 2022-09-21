495b8ed0436a09d798fb2b6e84fc2f6334180aad
Caballero018 <alejoxcaballero18@gmail.com>
2022-09-20 20:20:34 -0500

    Print all the letters except q and e

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 i = 97
 while i <= 122:
-    print("{}" .format(chr(i)), end="")
-    if i == 113 or i == 101
-        print("", end="")
-        i += 1
+    if not (i == 101 or i == 113):
+        print("{}" .format(chr(i)), end="")        
+    i += 1
 print("")
