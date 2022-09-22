cd423229bb6216a6379584fe98b35ea5c4767671
Caballero018 <alejoxcaballero18@gmail.com>
2022-09-22 17:56:37 -0500

    Program that imports the function def add(a, b):

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    import add_0
     a = 1
     b = 2
-    print("1 + 2 = {}" .format(__import__('add_0').add(a, b)))
+    print("1 + 2 = {}" .format(add_0.add(a, b)))
