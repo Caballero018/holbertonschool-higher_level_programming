c36c8c4541a397822b920a1525d41ff04a59441b
Caballero018 <alejoxcaballero18@gmail.com>
2022-09-23 01:42:09 -0500

    Program that prints all the names defined by the compiled module hidden_4.pyc

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 import hidden_4
 if __name__ == "__main__":
-   print(dir(hidden_4))
+    s = str(dir(hidden_4))
+    d = ""
+    for i in s:
+        if not(i == '\'' or i == '[' or i == ']' or i == ','):
+            d += i
+    d = d.split()
+    i = 9
+    while i < len(d):
+        print(d[i], end="\n")
+        i += 1
