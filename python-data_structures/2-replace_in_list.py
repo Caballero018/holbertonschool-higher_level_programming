5d61b485c0cae985ea6d1734f467561080077568
Caballero018 <alejoxcaballero18@gmail.com>
2022-09-25 16:49:39 -0500

    Function that replaces an element of a list at a specific position (like in C).

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    my_list[idx] = element
-    print("{:}" .format(my_list))
+    if not (idx < 0 or idx < len(my_list)):
+        my_list[idx] = element
+        return my_list
+    return my_list
     
