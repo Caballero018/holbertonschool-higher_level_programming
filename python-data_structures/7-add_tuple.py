0d9d43b4defcd05844afe4623a72236b4cb7c625
Caballero018 <alejoxcaballero18@gmail.com>
2022-09-27 01:16:32 -0500

    Function that adds 2 tuples.

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     tup = ()
-    tup2 = tuple_a + (0, 0) 
-    tup3 = tuple_b + (0, 0) 
+    tup2 = tuple_a + (0, 0)
+    tup3 = tuple_b + (0, 0)
     tup = tup2[0] + tup3[0], tup2[1] + tup3[1]
     return tup
-    
