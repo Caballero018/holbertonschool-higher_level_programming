1f685bf3a064d0a1b1636ed2c166fe4c1e023413
Caballero018 <alejoxcaballero18@gmail.com>
2022-09-27 22:06:34 -0500

    Function that deletes a key in a dictionary.

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -3,10 +3,8 @@ def simple_delete(a_dictionary, key=""):
     a = {}
     if a_dictionary != a:
         for i in a_dictionary.keys():
-            if key != a_dictionary[i]:
-                return a_dictionary
-        else:
-            del a_dictionary[key]
+            if key == a_dictionary[i]:
+                del a_dictionary[key]
             return a_dictionary
     
     
