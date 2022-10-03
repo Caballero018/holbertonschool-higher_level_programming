f290a5a284eff2cefc3270e122ecdb59f5a5d92b
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-03 00:57:27 -0500

    Class Square that defines a square by: (based on 2-square.py)

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
-"""
-Add area method in class Square.
-"""
+"Class Square that defines a square by: (based on 2-square.py)"
 
 
 class Square:
-    """
-    Class that defines a square.
-    Attributes:
-    size(int): size of the square - private attribute.
-    """
+    "Function with atrivute private"
     def __init__(self, size=0):
-        if type(size) != int:
-            raise TypeError('size must be an integer')
-        elif size < 0:
-            raise ValueError('size must be >= 0')
-        else:
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
             self.__size = size
-
+        else:
+            raise TypeError("size must be an integer")
     def area(self):
-        """Public instance method that returns the current square area"""
-        return self.__size ** 2
+        return self.__size
