74899c5cfc2e43bd70829693abe6ea939f99168b
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-03 00:48:57 -0500

    Class Square that defines a square by: (based on 1-square.py)

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 "Class Square that defines a square by: (based on 1-square.py)"
 
+
 class Square:
     "Function with atrivute private"
     def __init__(self, size=0):
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        if not (isinstance(size, int)):
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        else:
             raise TypeError("size must be an integer")
-        self.__size = size
