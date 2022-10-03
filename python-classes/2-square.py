29df66c08bb6a24668d16c951bc8b72ec245cc7f
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-03 00:46:04 -0500

    Class Square that defines a square by: (based on 1-square.py)

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
-"""
-Verifying attribute in class Square.
-"""
-
+"Class Square that defines a square by: (based on 1-square.py)"
 
 class Square:
-    """
-    Class that defines a square.
-    Attributes:
-    size(int): size of the square - private attribute.
-    """
-
+    "Function with atrivute private"
     def __init__(self, size=0):
-        if type(size) != int:
-            raise TypeError('size must be an integer')
-        elif size < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = size
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        if not (isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        self.__size = size
