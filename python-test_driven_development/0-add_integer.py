9e40e304306eefbacb61c7d3bfe9dfe0fa31b0b4
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-03 22:57:17 -0500

    Function that adds 2 integers.

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 "Function that adds 2 integers."
 
-
 def add_integer(a, b=98):
     if isinstance(a, float) or isinstance(b, float):
         a = int(a)
         b = int(b)
-    if a == None or not (isinstance(a, int)):
+    if not a  or not (isinstance(a, int)):
         raise TypeError("a must be an integer")
-    if b ==  None or not (isinstance(b, int)):
+    if not b or not (isinstance(b, int)):
         raise TypeError("b must be an integer")
     if isinstance(a, int) or isinstance(b, int):
         return a + b
+
+    if __name__ == "__main__":
+        import doctest
+        doctest.testmod()
