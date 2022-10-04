7920fe5414c4d75250eeeab77366dac8f78f4d5f
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-04 11:31:48 -0500

    Function that adds 2 integers.

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"Module that contains a function that adds 2 integers."
+"""Module that contains a function that adds 2 integers."""
 
 
 def add_integer(a, b=98):
-    "Function that adds 2 integers."
+    """Function that adds 2 integers."""
     if isinstance(a, float) or isinstance(b, float):
         a = int(a)
         b = int(b)
@@ -12,4 +12,5 @@ def add_integer(a, b=98):
     if not b or not (isinstance(b, int)):
         raise TypeError("b must be an integer")
     if isinstance(a, int) or isinstance(b, int):
+        """Add of int (a) and (b)"""
         return a + b
