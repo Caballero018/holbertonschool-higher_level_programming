6a11759ba270f00fbcd8803ddd16552c25cf509f
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-04 23:34:18 -0500

    Function that prints My name is <first name> <last name>

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -4,9 +4,11 @@
 
 def say_my_name(first_name, last_name=""):
     "Function that prints My name is <first name> <last name>"
-    if first_name or last_name:
-        if not isinstance(first_name, str):
-            raise TypeError("first_name must be a string")
-        if not isinstance(last_name, str):
-            raise TypeError("last_name must be a string")
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    if first_name == "" and last_name == "":
+        print("My name is")
+    else:
         print("My name is {} {}".format(first_name, last_name))
