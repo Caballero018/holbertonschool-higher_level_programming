75ced444340acbf728d6dd2a58a61d5fe3e9d74c
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-04 22:03:30 -0500

    Function that divides all elements of a matrix.

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -7,6 +7,8 @@ def matrix_divided(matrix, div):
     #    raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
+    if not (isinstance(div, int) or isinstance(div, float)):
+        raise TypeError("div must be a number")
     if type(matrix) is not list or (len(matrix) == 0) or type(matrix[0]) is not list or (len(matrix[0]) == 0):
         raise TypeError("matrix must be a matrix (list of lists) of \
 integers/floats")
