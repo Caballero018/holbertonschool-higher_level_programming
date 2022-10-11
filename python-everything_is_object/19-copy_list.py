aea0d4d17e5714fa01e40094bf4e179348ca8c34
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-11 16:24:05 -0500

    Function that return the copy of a list

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-"Module that have a function that return the copy of a list"
-
-
 def copy_list(l):
-    "Function that return the copy of a list"
     new_list = l[:]
     return new_list
