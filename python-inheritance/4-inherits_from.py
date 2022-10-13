3861d9668a8dd67b57a134d37ed95801eabc4488
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-13 11:32:47 -0500

    Function that returns True if the object is an instance of

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,9 +7,9 @@ def inherits_from(obj, a_class):
     try:
         if not obj and ((issubclass(obj, a_class) or isinstance(obj, a_class)) or (not issubclass(obj, a_class) or not isinstance(obj, a_class))):
             return False
-        if obj and type(obj) is a_class and issubclass(obj, a_class):
-            return False
-        else:
+        if obj and type(obj) is not a_class and issubclass(obj, a_class):
             return True
+        else:
+            return False
     except TypeError:
             return True
