277ea448bedd061578e1dc2b0998b7c4700f6e33
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-12 19:24:46 -0500

    Function that returns the list of available attributes and methods of an object

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -4,6 +4,5 @@
 
 def lookup(obj):
     "Doc"
-    object_methods = [method_name for method_name in dir(obj)
-                  if callable(getattr(obj, method_name))]
-    return object_methods
+    obj = dir(obj)
+    return obj
