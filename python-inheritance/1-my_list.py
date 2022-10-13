f9ed43b13d677ce247d94ef33d3edff005981b6d
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-12 20:19:20 -0500

    Class MyList that inherits from list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"Module that have a class MyList that inherits from list"
+
+
 class MyList(list):
+    "Class MyList that inherits from list"
     def print_sorted(self):
         self.sort()
         print(self)
