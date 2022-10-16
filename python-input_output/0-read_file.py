37a7929a14f7c40c72c729abe065a399de74188f
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-16 18:10:15 -0500

    Function that reads a text file (UTF8) and prints it to stdout

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,6 +6,5 @@ text file (UTF8) and prints it to stdout"
 def read_file(filename=""):
     "Function that reads a text file (UTF8) and prints it to stdout"
     with open(filename, encoding="utf-8") as f:
-        read_file = f.read()
-        
+        read_file = f.read()   
     print(read_file, end="")
