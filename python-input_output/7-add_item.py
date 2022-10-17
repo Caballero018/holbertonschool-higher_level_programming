7017328a933f297214742f21ec715f2980b804f2
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-16 23:17:09 -0500

    Script that adds all arguments to a Python list, and then save them to a file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"Script that adds all arguments to a Python list, and then save them to a file"
+"Script that adds all arguments to a Python list, and then save them to file"
 from sys import argv
 
 
