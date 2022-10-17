e60b5e7283c59676d5461a1cf02b907b4d1b54b6
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-17 18:19:06 -0500

    Function to make tryangle pascual

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
-""
+"""Function pascal form"""
 
 
 def pascal_triangle(n):
-    ls = []
-    if n > 0:
-        ls.append([1])
-        for r in range(1, n):
-            pr = ls[-1]
-            ls.append([1] + [pr[i - 1] + pr[i] for i in range(1, r)] + [1])
-    return ls
+    """Function that returns a list of lists of
+    integers representing the Pascal’s form of n"""
+    form = []
+    if n <= 0:
+        return form
+    elif n == 1:
+        form.append([1])
+    elif n == 2:
+        form.append([1])
+        form.append([1, 1])
+    else:
+        form.append([1])
+        form.append([1, 1])
+        for i in range(2, n):
+            form.append(
+                [form[i-1][j-1] + form[i-1][j]
+                    if j != 0 and i != j else 1 for j in range(0, i+1)]
+            )
+    return form
