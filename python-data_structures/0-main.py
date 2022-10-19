f5b91e8a7b417b991deedfbd71882e8b8ba6f2a3
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-19 15:45:11 -0500

    Changes

--- python-data_structures/0-main.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-list = [0, 1, 2, 2]
-print(list)
-list[2] = 9
-
--- python-inheritance/4-main.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-inherits_from = __import__('4-inherits_from').inherits_from
-
-a = None
-print(inherits_from(a, list))
