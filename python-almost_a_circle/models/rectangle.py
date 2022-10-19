e16f912d7f81ef194d3cb2fd4c22b53e56cc8c34
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-19 11:20:03 -0500

    Class Rectangle that inherits from Base

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"Module that have a class Rectangle that inherits from Base"
+from models.base import Base
+
+class Rectangle(Base):
+    "Class Rectangle that inherits from Base"
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    "width"
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self, value):
+        pass
+
+    "height"
+    @property
+    def height(self):
+        return self.__height
+    @height.setter
+    def height(self, value):
+        pass
+
+    "x"
+    @property
+    def x(self):
+        return self.__x
+    @x.setter
+    def x(self, value):
+        pass
+
+    "y"
+    @property
+    def y(self):
+        return self.__y
+    @y.setter
+    def y(self, value):
+        pass
