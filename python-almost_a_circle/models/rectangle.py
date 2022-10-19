5e972a48662a1842d51c19a0645f3263151879d3
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-19 16:38:23 -0500

    Class Rectangle that inherits from Base

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -86,5 +86,7 @@ class Rectangle(Base):
 
     def __str__(self):
         "__str__"
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(\
-self.id, self.__x, self.__y, self.__width, self.__height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+                self.id, self.__x,
+                self.__y, self.__width, self.__height
+            )
