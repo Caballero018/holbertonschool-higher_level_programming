2a6123ef387b5983e784ceada960225780109e20
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-19 14:41:06 -0500

    Class Rectangle that inherits from Base

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,62 +1,71 @@
 #!/usr/bin/python3
 "Module that have a class Rectangle that inherits from Base"
+from turtle import width
 from models.base import Base
 
 
 class Rectangle(Base):
     "Class Rectangle that inherits from Base"
     def __init__(self, width, height, x=0, y=0, id=None):
+        "Class constructor"
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
 
-    "width"
+
     @property
     def width(self):
+        "width"
         return self.__width
 
     @width.setter
     def width(self, value):
+        "width"
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
-    "height"
+
     @property
     def height(self):
+        "height"
         return self.__height
 
     @height.setter
     def height(self, value):
+        "height"
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
-    "x"
+
     @property
     def x(self):
+        "x"
         return self.__x
 
     @x.setter
     def x(self, value):
+        "x"
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
-    "y"
     @property
     def y(self):
+        "y"
         return self.__y
 
     @y.setter
     def y(self, value):
+        "y"
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
