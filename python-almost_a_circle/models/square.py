80bf4e4c186fe0738c6843f47a107dcec4aa11ff
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-20 16:53:43 -0500

    Class Square that inherits from Rectangle

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -28,4 +28,4 @@ class Square(Rectangle):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-        self.size = value
+        self.__size = value
