7028caf80c70fdd8c3c3edc177e16e76f14fb2a5
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-20 21:22:29 -0500

    Module that have a class named Base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 "Module that have a class named Base"
+import json
 
 
 class Base:
@@ -7,8 +8,18 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        Class constructor
+        """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+        Static method that returns the JSON string representation
+        of list_dictionaries
+        """
+        return json.dumps(list_dictionaries)
