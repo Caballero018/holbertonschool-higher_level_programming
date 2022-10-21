f19324574b825f9ef874b489b0143bd574bdfd21
Caballero018 <alejoxcaballero18@gmail.com>
2022-10-21 10:30:07 -0500

    Class named Base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -25,10 +25,10 @@ class Base:
         """
         if list_dictionaries is None:
             return "[]"
-        return json.dumps(list_dictionaries)
+        return json.dumps([i.__dict__ for i in list_dictionaries])
 
     @classmethod
     def save_to_file(cls, list_objs):
-        for ls in list_objs:
-            return cls(ls)
-
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as f:
+            f.write(cls.to_json_string(list_objs))
