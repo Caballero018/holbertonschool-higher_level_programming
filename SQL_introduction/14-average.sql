fcc385f33178b9be12cde2c819408912a6edf085
Caballero018 <alejoxcaballero18@gmail.com>
2022-11-11 14:36:49 -0500

    Script that computes the score average of all records in the table second_table.

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,4 +1,2 @@
 -- Script that computes the score average of all records in the table second_table.
-ALTER TABLE second_table ADD average INT;
-INTO INSERT second_table (average) VALUES (avg(score));
-SELECT average FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
