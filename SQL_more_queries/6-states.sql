f74d233e838bf69ad98dbe657ded4cbca0dbb2d3
Caballero018 <alejoxcaballero18@gmail.com>
2022-11-16 12:18:09 -0500

    Script that creates the database hbtn_0d_usa and the table states.

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- Script that creates the database hbtn_0d_usa and the table states.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa.states' (
     id INT UNIQUE NOT NULL AUTO_INCREMENT,
 );
