b4a98b9e97319970ae5f4aa124805bdb409584c0
Caballero018 <alejoxcaballero18@gmail.com>
2022-11-17 17:52:28 -0500

    Script that lists all cities from the database hbtn_0e_4_usa

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"Module that lists all states from the database hbtn_0e_0_usa"
+import MySQLdb
+import sys
+
+
+"Condition that causes the file cannot be imported"
+if __name__ == "__main__":
+    argv = sys.argv
+
+    "Connect python file to server"
+    db = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=argv[1], passwd=argv[2],
+        db=argv[3], charset='utf8'
+        )
+
+    "Start the cursor"
+    cur = db.cursor()
+
+    "Execute the query"
+    cur.execute(
+        """
+        SELECT cities.id, cities.name, states.name FROM cities
+        INNER JOIN states ON states.id = cities.state_id ORDER BY
+        cities.id ASC
+        """
+    )
+    rows = cur.fetchall()
+
+    "Print the rows of the table"
+    for row in rows:
+        print(row)
+
+    "Close all cursors"
+    cur.close()
+    "Close all databases"
+    db.close()
