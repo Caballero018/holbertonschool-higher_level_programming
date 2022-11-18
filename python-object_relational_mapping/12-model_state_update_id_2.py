acd8a4b0f961ff6929278269636edfe4de12445f
Caballero018 <alejoxcaballero18@gmail.com>
2022-11-18 16:29:14 -0500

    Module that update lists

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"Module that lists all states from the database hbtn_0e_6_usa"
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+def state():
+    session = Session()
+
+    qry = session.query(State).filter(State.id == 2).first()
+    qry.name = "New Mexico"
+    session.commit
+    session.close()
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+                    "mysql+mysqldb://{}:{}@localhost/{}".format(
+                                        sys.argv[1], sys.argv[2],
+                                        sys.argv[3]), pool_pre_ping=True
+                                        )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    state()
