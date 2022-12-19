f07f444a1432fd125b7cb1d405ddb832ece4dbcc
Caballero018 <4617@holbertonstudents.com>
2022-12-19 11:46:21 -0500

    Script that prints two arguments passed to it

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 const args = process.argv;
 let message;
-let len = Object.keys(args).length;
+const len = Object.keys(args).length;
 
 if (len === 3) {
-    message = `${args[2]} is undefined`
-    console.log(message);
+  message = `${args[2]} is undefined`;
+  console.log(message);
 } else if (len === 4) {
-    message = `${args[2]} is ${args[3]}`
-    console.log(message);
+  message = `${args[2]} is ${args[3]}`;
+  console.log(message);
 } else {
-    message = 'undefined is undefined';
-    console.log(message);
+  message = 'undefined is undefined';
+  console.log(message);
 }
