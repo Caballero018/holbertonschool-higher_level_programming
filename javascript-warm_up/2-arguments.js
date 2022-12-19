2510c4c0b0faa4988314df66ae7c5c390918111e
Caballero018 <4617@holbertonstudents.com>
2022-12-19 09:32:46 -0500

    Script that prints a message depending of the number of arguments passed

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,10 +2,10 @@
 const myVar = process.argv;
 let message;
 
-if (Object.keys(myVar).length > 2) {
+if (Object.keys(myVar).length == 2) {
   message = 'Argument found';
   console.log(message);
-} else if (Object.keys(myVar).length > 3) {
+} else if (Object.keys(myVar).length >= 3) {
   message = 'Arguments found';
   console.log(message);
 } else {
