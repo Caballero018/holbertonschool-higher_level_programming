7754a2728967c850c1a37cfed1bdaa12f2ed3019
Caballero018 <4617@holbertonstudents.com>
2022-12-22 17:03:47 -0500

    Script that reads and prints the content of a file.

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', function (error, data) {
-    if (data) {
-        console.log(data);
-    } else {
-        console.error(error);
-    }
+  if (data) {
+    console.log(data);
+  } else {
+    console.error(error);
+  }
 });
