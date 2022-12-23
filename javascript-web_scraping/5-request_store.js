33c19a19e4c8b82b2ee86d6665a04ea67e2ae01d
Caballero018 <4617@holbertonstudents.com>
2022-12-22 22:55:08 -0500

    Script that gets the contents of a webpage and stores it in a file.

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -2,10 +2,13 @@
 const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
-const path = './' + process.argv[3];
 request.get({ url: url }, function (error, response, body) {
   if (!error && response.statusCode === 200) {
-    if (Object.keys(body).length > 0 && (!fs.existsSync(path.toString()))) {
+    if (Object.keys(body).length > 0) {
+      fs.writeFile(process.argv[3], '', 'utf-8', (err) => {
+        if (err) throw err;
+      });
+    } else {
       fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
         if (err) throw err;
       });
