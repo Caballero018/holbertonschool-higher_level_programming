2705ea0fdcea6f71ccf8bced727b9b2c11e8ec2d
Caballero018 <4617@holbertonstudents.com>
2022-12-23 00:05:25 -0500

    Script that computes the number of tasks completed by user id.

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -5,16 +5,15 @@ request.get({ url: url, json: true }, function (error, response, body) {
   if (!error) {
     const obj = {};
     let count = 1;
-    let i = 0;
     // eslint-disable-next-line no-unused-vars
-    for (const ds in body) {
-      i++;
+    for (let i = 1; i <= Object.keys(body).length; i++) {
       for (const dic of body) {
         if (i === dic.userId) {
           if (dic.completed === true) obj[i.toString()] = count++;
         }
       }
       count = 1;
+      console.log(i);
     }
     console.log(obj);
   }
