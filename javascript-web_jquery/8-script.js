d4473790917beea5e35a0f637b72f7c174cbaa30
Caballero018 <4617@holbertonstudents.com>
2022-12-27 17:05:01 -0500

    Script that fetches and lists the title for all movies by using this URL: https://swapi-api.hbtn.io/api/films/?format=json

---
+++ javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
+$.get(url, function (datas, textStatus, jqXHR) {
+  for (data of datas.results) $('#list_movies').append('<li>' + data.title + '</li>');
+});
